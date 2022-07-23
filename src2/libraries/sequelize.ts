18efb5d303034a6273d3f06b140474cdbc3f7234
Dong H. Kim <98877622+donghquinn@users.noreply.github.com>
2022-07-23 15:32:46 +0900

    Update sequelize.ts

--- src2/libraries/sequelize.ts
+++ src2/libraries/sequelize.ts
@@ -6,17 +6,9 @@ import { Sequelize } from 'sequelize';
 
 
 // connect to the Database
-// export const initSequelize = new Sequelize(process.env.DATABASE, process.env.USER, process.env.PASSWORD, {
-//   host: process.env.HOST,
-//   port: Number(process.env.PORT),
-//   dialect: 'mariadb',
-//   logging: console.log
-// });
-
-
-export const initSequelize = new Sequelize('test', 'root', 'samquinn123!', {
-  host: '141.164.56.120',
-  port: 3306,
+export const initSequelize = new Sequelize(process.env.DATABASE, process.env.USER, process.env.PASSWORD, {
+  host: process.env.HOST,
+  port: Number(process.env.PORT),
   dialect: 'mariadb',
   logging: console.log
 });
